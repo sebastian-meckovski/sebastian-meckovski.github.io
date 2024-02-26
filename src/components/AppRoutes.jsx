732e618c7fbb9008a4fb262ccb0246f9ee5638d8
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2024-02-26 15:39:20 +0000

    home page improvements

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import { Contact } from "../pages/Contact";
+import { AboutMe } from "../pages/AboutMe";
 
 export const AppRoutes = () => {
     return (
         <main>
             <Routes>
-                <Route index element={<p>home</p>} />
+                <Route index element={<AboutMe />} />
                 <Route path="about-me" element={<p>about me</p>} />
                 <Route path="skills" element={<p>skills</p>} />
                 <Route path="contact" element={<Contact />} />
