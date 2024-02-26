95932a1f2348f9473a6ecd251e00ec639f751702
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2024-02-26 15:41:39 +0000

    home page improvements

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,8 +4,8 @@ import sebImage from '../assets/PXL_20240226_132844396.NIGHT.jpg'
 export const AboutMe = () => {
     return (
         <section className="about-me">
-            <img className="about-me-image" src={sebImage} alt="Seb's picture" />
-            <h1>Hey, Seb here</h1>
+            <img className="about-me-image" src={sebImage} alt="Sebastian" />
+            <h1>Hi, Seb here</h1>
             <h2 className="placeholder">
                 I am a&nbsp;
                 <span className="typewriter"></span>
