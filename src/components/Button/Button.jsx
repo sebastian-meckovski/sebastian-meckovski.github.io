a13afc0df6dfdde475bc238e73243924e7b1c71b
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2024-02-26 22:41:09 +0000

    home page improvements

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './Button.scss'
+
+
+export const Button = () =>{
+    return (
+        <button className="custom-button"> Hire Me </button>
+    )
+}
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import sebImage from '../assets/PXL_20240226_132844396.NIGHT.jpg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faFacebook, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Button/Button";
 
 export const AboutMe = () => {
     return (
-        <section className="about-me">
-            <img className="about-me-image" src={sebImage} alt="Sebastian" />
-            <h1>Hi, Seb here</h1>
-            <h2 className="placeholder">
-                I am a&nbsp;
-                <span className="typewriter"></span>
-            </h2>
-        </section>
+        <>
+            <section>
+                <img className="about-me-image" src={sebImage} alt="Sebastian" />
+                <h1>Hi, Seb here</h1>
+                <h2 className="placeholder">
+                    I am a&nbsp;
+                    <span className="typewriter"></span>
+                </h2>
+            </section>
+            <section>
+
+                <p> A versatile software developer with 3 years of experience in frontend, backend, testing, and web technologies. Passionate about learning and implementing new technologies.</p>
+                <div className="icons-container">
+                    <Link target="_blank" to={'https://www.linkedin.com/in/sebastian-meckovski/'}>
+                        <FontAwesomeIcon className="icon-class" icon={faLinkedin} />
+                    </Link>
+                    <Link target="_blank" to={'https://www.facebook.com/sebastian.meckovski'}>
+                        <FontAwesomeIcon className="icon-class" icon={faFacebook} />
+                    </Link>
+                    <Link target="_blank" to={'https://www.instagram.com/sebastian_meckovski/'}>
+                        <FontAwesomeIcon className="icon-class" icon={faInstagram} />
+                    </Link>
+                    <Link target="_blank" to={'https://github.com/sebastian-meckovski'}>
+                        <FontAwesomeIcon className="icon-class" icon={faGithub} />
+                    </Link>
+                </div>
+                <Button />
+            </section>
+        </>
     )
 }
