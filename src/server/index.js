55ce99bb41e26475e59ca7d0eafb1a0339fe97da
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2024-03-08 13:52:21 +0000

    backend updarted for email service

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
-const PORT = 8000;
 const express = require("express");
 const yup = require("yup");
-require("dotenv").config();
 const cors = require("cors");
-// const axios = require("axios");
+const nodemailer = require("nodemailer");
+const PORT = 8000;
+require("dotenv").config();
 
 const allowedOrigins = [
   "http://localhost:3000",
@@ -52,6 +52,32 @@ const validate = (schema) => async (req, res, next) => {
 };
 
 app.post("/send-mail", validate(schema), (req, res) => {
+  console.log("sending email...");
+  let transporter = nodemailer.createTransport({
+    host: process.env.REACT_APP_HOST,
+    port: process.env.PORT,
+    auth: {
+      user: process.env.REACT_APP_EMAIL_LOGIN,
+      pass: process.env.REACT_APP_EMAIL_PASS
+    },
+  });
+
+  var mailOptions = {
+    from: process.env.REACT_APP_EMAIL_LOGIN,
+    to: req.body.email,
+    subject: req.body.subject,
+    // text: req.body.message,
+    html: `<h1>Hello, ${req.body.name}! </h1> Thanks for reaching out! 
+    This page is still under developemnt but I will get back to you shortly!`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+  });
+
   return res.json({ body: req.body });
 });
 
