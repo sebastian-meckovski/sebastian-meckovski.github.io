2dd5480e4361f2dba14ce9813afd7a35847afba3
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2024-03-09 09:10:53 +0000

    frontend test using sendgrid

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const yup = require("yup");
 const cors = require("cors");
-// const nodemailer = require("nodemailer");
 const PORT = 8000;
 require("dotenv").config();
-// const emailjs = require("@emailjs/nodejs");
 const sgMail = require("@sendgrid/mail");
-
-var templateParams = {
-  name: "James",
-  notes: "Check this out!",
-};
 
 const allowedOrigins = [
   "http://localhost:3000",
@@ -63,13 +56,13 @@ app.post("/send-mail", validate(schema), (req, res) => {
   sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
   const msg = {
     to: req.body.email,
-    from: process.env.REACT_APP_FROM, // Use the email address or domain you verified above
+    from: process.env.REACT_APP_FROM,
     subject: req.body.subject,
     // text: req.body.message,
     html: `<h1>Hi, ${req.body.name}!</h1> <p> Thanks for reaching out!
     This page is still under developemnt but I will get back to you shortly </p>`,
   };
-  //ES6
+
   (async () => {
     try {
       await sgMail.send(msg).then((obj) => {
@@ -78,55 +71,11 @@ app.post("/send-mail", validate(schema), (req, res) => {
       });
     } catch (error) {
       console.error(error);
-
       if (error.response) {
         console.error(error.response.body);
       }
     }
   })();
-
-  // emailjs
-  //   .send(
-  //     process.env.REACT_APP_SERVICE_ID,
-  //     process.env.REACT_APP_TEMPLATE_ID,
-  //     templateParams,
-  //     {
-  //       publicKey: process.env.REACT_APP_PUBLIC_KEY,
-  //       privateKey: process.env.REACT_APP_PRIVATE_KEY, // optional, highly recommended for security reasons
-  //     }
-  //   )
-  //   .then(
-  //     function (response) {
-  //       console.log("SUCCESS!", response.status, response.text);
-  //     },
-  //     function (err) {
-  //       console.log("FAILED...", err);
-  //     }
-  //   );
-
-  // let transporter = nodemailer.createTransport({
-  //   host: process.env.REACT_APP_HOST,
-  //   port: process.env.PORT,
-  //   auth: {
-  //     user: process.env.REACT_APP_EMAIL_LOGIN,
-  //     pass: process.env.REACT_APP_EMAIL_PASS,
-  //   },
-  // });
-
-  // var mailOptions = {
-  //   from: process.env.REACT_APP_EMAIL_LOGIN,
-  //   to: req.body.email,
-  //   subject: req.body.subject,
-  //   // text: req.body.message,
-  //   html: `<h1>Hello, ${req.body.name}! </h1> Thanks for reaching out!
-  //   This page is still under developemnt but I will get back to you shortly!`,
-  // };
-  // transporter.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-  //   console.log("Message sent: %s", info.messageId);
-  // });
 
   return res.json({ body: req.body });
 });
